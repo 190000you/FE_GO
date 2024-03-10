c8d5aacde25c8005bfe0b913dd4451ddcfe80e10
Lee_jinhyeok <121598818+jinhuck854@users.noreply.github.com>
2024-03-11 00:18:36 +0900

    로그인 & 회원가입
    1. 로그인
     1) UI 변경
     2) 애니메이션 추가
    
    2. 회원가입
     1) 이메일 유효성 추가 // yaml 파일 
     2) 인증 추가
     3) 비밀번호 체크
     4) 회원가입 조건 추가 (글자수, 빈공간, 비밀번호 일치)
     5) 회원가입 성공 시, "성공" 팝업 창
     6) 회원가입 실패 시, 실패한 이유 알려줌

--- App_Test_jinhyeok/login.dart
+++ App_Test_jinhyeok/login.dart
@@ -26,8 +26,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class LoginPage extends StatefulWidget { // StatefulWidget : User 정보 반영하기 위해
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
 // 로그인 페이지
-class LoginPage extends StatelessWidget {
+class LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,135 +42,163 @@ class LoginPage extends StatelessWidget {
         title: SizedBox.shrink(),
       ),
       // body
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          SizedBox(height: 100),
-          // 1. 로그인 위 글자 삽입함 : Let's go?
-          Text(
-            'Let\'s go?', // 폰트는 나중에 통일하기
-            style: GoogleFonts.oleoScript(fontSize: 36),
-          ),
-          // 2. ID 입력칸 : 관리자 == admin
-          SizedBox(height: 20),
-          TextField(
-            decoration: InputDecoration(
-              labelText: '아이디',
-              border: OutlineInputBorder(),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            SizedBox(height: 100),
+            // 1. 로그인 위 글자 삽입함 : Let's go?
+            Text(
+              'Let\'s go?', // 폰트는 나중에 통일하기
+              style: GoogleFonts.oleoScript(fontSize: 36),
             ),
-          ),
-          // 3. PW 입력칸 : 관리자 == 1234
-          SizedBox(height: 20),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: '비밀번호',
-              border: OutlineInputBorder(),
-            ),
-          ),
-          // 4. 로그인 버튼
-          SizedBox(height: 20),
-          SizedBox(
-            width: double.infinity,
-            height: 60, // 로그인 버튼 높이 조정
-            child: ElevatedButton(
-              onPressed: () {
-                // 아이디와 비밀번호가 admin, 1234일 때 mainpage로 이동
-                if (_checkLogin("admin", "1234")) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MainPage()),
-                  );
-                } else {
-                  // 로그인 실패 시 처리
-                  print("로그인 실패");
-                }
-              },
-              child: Text(
-                '로그인',
-                style: TextStyle(fontSize: 18),
+            // 2. ID 입력칸 : 관리자 == admin
+            SizedBox(height: 20),
+            TextFormField(
+              key: const ValueKey(1), // ID 오류
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.person,
+                  color: Colors.grey,
+                ),
+                labelText: '아이디',
+                contentPadding: EdgeInsets.all(10),
+                border: OutlineInputBorder(),
               ),
             ),
-          ),
-          // 5. 회원가입 버튼
-          SizedBox(height: 10), // 간격 조정
-          Row(
-            mainAxisAlignment:
-                MainAxisAlignment.spaceBetween, // 회원가입 버튼 : 왼쪽과 오른쪽으로 정렬
-            children: [
-              SizedBox(
-                // 회원가입 버튼 크기 조정
-                width: 140, // 버튼 너비 조정
-                height: 40, // 버튼 높이 조정
-                child: ElevatedButton(
-                  onPressed: () {
-                    // 회원가입 버튼 눌렀을 때의 동작 추가
+            // 3. PW 입력칸 : 관리자 == 1234
+            SizedBox(height: 20),
+            TextFormField(
+              obscureText: true,
+              key: const ValueKey(2),
+              decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Colors.grey,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),
+                ),
+                hintText: '비밀번호',
+                hintStyle: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+                contentPadding: EdgeInsets.all(10),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            // 4. 로그인 버튼
+            SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              height: 60, // 로그인 버튼 높이 조정
+              child: ElevatedButton(
+                onPressed: () {
+                  // 아이디와 비밀번호가 admin, 1234일 때 mainpage로 이동
+                  if (_checkLogin("admin", "1234")) {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => SignUpPage()),
+                      MaterialPageRoute(builder: (context) => MainPage()),
                     );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.transparent, // 버튼 배경색 투명하게 설정
-                    elevation: 0, // 그림자 없애기
-                  ),
-                  child: Text(
-                    '회원가입',
-                    style: TextStyle(fontSize: 14),
-                  ),
+                  } else {
+                    // 로그인 실패 시 처리
+                    print("로그인 실패");
+                  }
+                },
+                child: Text(
+                  '로그인',
+                  style: TextStyle(fontSize: 18),
                 ),
               ),
-              // 6. 계정 찾기 버튼
-              SizedBox(
-                // 계정 찾기 버튼 크기 조정
-                width: 140, // 버튼 너비 조정
-                height: 40, // 버튼 높이 조정
-                child: ElevatedButton(
-                  onPressed: () {
-                    // 계정 찾기 버튼 눌렀을 때의 동작 추가
-                    // 계정 찾기 페이지 생성 후 페이지 이동
+            ),
+            // 5. 회원가입 버튼
+            SizedBox(height: 10), // 간격 조정
+            Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.spaceBetween, // 회원가입 버튼 : 왼쪽과 오른쪽으로 정렬
+              children: [
+                SizedBox(
+                  // 회원가입 버튼 크기 조정
+                  width: 140, // 버튼 너비 조정
+                  height: 40, // 버튼 높이 조정
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // 회원가입 버튼 눌렀을 때의 동작 추가
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent, // 버튼 배경색 투명하게 설정
+                      elevation: 0, // 그림자 없애기
+                    ),
+                    child: Text(
+                      '회원가입',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ),
+                ),
+                // 6. 계정 찾기 버튼
+                SizedBox(
+                  // 계정 찾기 버튼 크기 조정
+                  width: 140, // 버튼 너비 조정
+                  height: 40, // 버튼 높이 조정
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // 계정 찾기 버튼 눌렀을 때의 동작 추가
+                      // 계정 찾기 페이지 생성 후 페이지 이동
 
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => FindIdPage()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.transparent, // 버튼 배경색 투명하게 설정
-                    elevation: 0, // 그림자 없애기
-                  ),
-                  child: Text(
-                    '계정 찾기',
-                    style: TextStyle(fontSize: 14),
-                  ),
-                ),
-              ),
-              SizedBox(
-                // 7. 비밀번호 찾기 버튼
-                width: 140, // 버튼 너비 조정
-                height: 40, // 버튼 높이 조정
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => FindPwPage()),
-                    );
-                    // 비밀번호 찾기 버튼 눌렀을 때의 동작 추가
-                    // 비밀번호 찾기 페이지 생성 후 페이지 이동
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.transparent, // 버튼 배경색 투명하게 설정
-                    elevation: 0, // 그림자 없애기
-                  ),
-                  child: Text(
-                    '비밀번호 찾기',
-                    style: TextStyle(fontSize: 14),
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => FindIdPage()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent, // 버튼 배경색 투명하게 설정
+                      elevation: 0, // 그림자 없애기
+                    ),
+                    child: Text(
+                      '계정 찾기',
+                      style: TextStyle(fontSize: 14),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ]),
+                SizedBox(
+                  // 7. 비밀번호 찾기 버튼
+                  width: 140, // 버튼 너비 조정
+                  height: 40, // 버튼 높이 조정
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => FindPwPage()),
+                      );
+                      // 비밀번호 찾기 버튼 눌렀을 때의 동작 추가
+                      // 비밀번호 찾기 페이지 생성 후 페이지 이동
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent, // 버튼 배경색 투명하게 설정
+                      elevation: 0, // 그림자 없애기
+                    ),
+                    child: Text(
+                      '비밀번호 찾기',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ]),
+        ),
       ),
     );
   }
--- App_Test_jinhyeok/signUp.dart
+++ App_Test_jinhyeok/signUp.dart
@@ -1,5 +1,6 @@
 // 회원가입 페이지
 import 'package:flutter/material.dart';
+import 'package:go_test_ver/login.dart';
 
 // import : 
 import 'package:google_fonts/google_fonts.dart'; // google 폰트
@@ -21,8 +22,38 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class SignUpPage extends StatefulWidget { // Stateful : 유저 화면에도 정보 반영
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
 // 회원가입 페이지
-class SignUpPage extends StatelessWidget {
+class _SignUpPageState extends State<SignUpPage> { 
+  // Form Key
+  final formKey = GlobalKey<FormState>(); // formKey 정의
+  bool isOk = false;
+
+  // 변수
+  String userEmail = "";
+  String userName = "";
+  String userId = "";
+  String userPassword = "";
+  String userPasswordCheck = "";
+
+
+  // 인증
+  void tryValidation() {
+    final isValid = formKey.currentState!.validate(); // 인증된 상태 여부 확인
+
+    if (isValid) { // 인증 완료
+      formKey.currentState!.save();
+      isOk = true;
+    }
+    else{ // 인증 실패
+      print("인증 실패");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +71,9 @@ class SignUpPage extends StatelessWidget {
       // body : Padding
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
+        child: Form(
+          key: formKey,
+          child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // 1. 회원가입 위 글자 삽입
@@ -52,65 +85,171 @@ class SignUpPage extends StatelessWidget {
             ),
             // 2. 이메일 인증
             SizedBox(height: 20),
-            TextField(
-              obscureText: true,
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty || value.length < 2) {
+                  // 이메일 입력란이 비어있으면 '이메일을 입력해주세요' 리턴
+                  return "이메일을 입력해주세요";
+                } else if (!EmailValidator.validate(value.toString())) {
+                  // 입력값이 이메일 형식에 맞지 않으면 '이메일 형식을 맞춰주세요를 리턴
+                  return "이메일 형식을 맞춰주세요";
+                } else {
+                  return null;
+                }
+              },
+              onSaved: (value) async { // 이메일 저장
+                userEmail = value!;
+              },
+              onChanged: (value) {
+                userEmail = value;
+              },
+              // obscureText: true, // *로 표시 설정/해제
               decoration: InputDecoration(
                 labelText: '이메일 입력',
                 border: OutlineInputBorder(),
               ),
+              key: const ValueKey(1), // 이메일 오류
             ),
             // 3. 이름 입력칸
             SizedBox(height: 20),
-            TextField(
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty) {
+                  // 이름 입력란이 비어 있을 때
+                  return "이름을 입력해주세요";
+                } else {
+                  return null;
+                }
+              },
+              onSaved: (value) { // 이름 저장
+                userName = value!;
+              },
+              onChanged: (value) {
+                userName = value;
+              },
               decoration: InputDecoration(
                 labelText: '이름',
                 border: OutlineInputBorder(),
               ),
+              key: const ValueKey(2), // 이름 오류
             ),
             // 4. ID 입력칸
             SizedBox(height: 20),
-            TextField(
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty || value.length < 6) {
+                  // ID 입력란이 비어 있거나, ID가 설정해둔 자릿수 미만이면, '최소 6자리로 ID를 설정해주세요' 리턴
+                  return "최소 6자리로 아이디를 설정해주세요";
+                } else {
+                  return null;
+                }
+              },
+              onSaved: (value) { // Id 저장
+                userId = value!;
+              },
+              onChanged: (value) {
+                userId = value;
+              },
               decoration: InputDecoration(
                 labelText: '아이디',
                 border: OutlineInputBorder(),
               ),
+              key: const ValueKey(3), // ID 오류
             ),
             // 5. PW 입력칸
             SizedBox(height: 20),
-            TextField(
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty || value.length < 6) {
+                  // 비밀번호 입력란이 비어 있거나, 비밀번호가 설정해둔 자릿수 미만이면, '최소 6자리로 비밀번호를 설정해주세요' 리턴
+                  return "최소 6자리로 비밀번호를 설정해주세요";
+                } else {
+                  return null;
+                }
+              },
+              onSaved: (value) {
+                userPassword = value!;
+              },
+              onChanged: (value) {
+                userPassword = value;
+              },
               obscureText: true,
               decoration: InputDecoration(
                 labelText: '비밀번호',
                 border: OutlineInputBorder(),
               ),
+              key: const ValueKey(4), // PW 오류
             ),
             // 6. 비밀번호 재입력
             SizedBox(height: 20),
-            TextField(
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty || value.length < 6 || value != userPassword) {
+                  return '비밀번호를 다시 확인해주세요.';
+                } else {
+                  return null;
+                }
+              },
+              onSaved: (value) {
+                userPasswordCheck = value!;
+              },
+              onChanged: (value) {
+                userPasswordCheck = value;
+              },
               obscureText: true,
               decoration: InputDecoration(
                 labelText: '비밀번호 재입력',
                 border: OutlineInputBorder(),
               ),
+              key: const ValueKey(5), // PW 확인 오류
             ),
-            
-            /* 다른 페이지에서 진행
-            // 5. 닉네임 입력칸
-            SizedBox(height: 20),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '사용자 닉네임',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            */
             // 6. 회원가입 버튼
             SizedBox(height: 20),
             SizedBox(
               height: 60,
               width: double.infinity,
               child: ElevatedButton(
-                onPressed: () {},
+                onPressed: () {
+                  tryValidation(); // 인증 진행
+                  // 인증 완료 시, user 정보 출력
+                  print(userEmail);
+                  print(userName);
+                  print(userId);
+                  print(userPassword);
+                  print(userPasswordCheck);
+                  // 이메일 패스워드 확인
+                  if (userEmail != '' && userName != '' && userId != '' && userPassword != '' && userPasswordCheck != ''   && isOk == true) {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('회원가입 성공'),
+                          content: Text('회원가입에 성공했습니다.'),
+                          actions: [
+                            TextButton(
+                              child: Text('확인'),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => LoginPage()),
+                                );
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    ).then((value) {
+                      // 확인 버튼을 누른 후에 로그인 페이지로 이동
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                      );
+                    });
+                  }
+                  // 실패 시, 로그인 창으로 이동하는데, 팝업창 띄우기로? 
+                  else {
+                  }
+                },
                 child: Text(
                   '회원가입',
                   style: TextStyle(fontSize: 18),
@@ -119,6 +258,7 @@ class SignUpPage extends StatelessWidget {
             ),
           ],
         ),
+      ),
       ),
     );
   }

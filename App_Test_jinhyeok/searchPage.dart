96defa40853c3dc52c25bbbf170e94481d702d99
rlaahrwhd <103015094+rlaahrwhd@users.noreply.github.com>
2024-04-06 14:32:07 +0900

    Update searchPage.dart

--- App_Test_jinhyeok/searchPage.dart
+++ App_Test_jinhyeok/searchPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/searchPage_info.dart'; // 경로 설정.
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -22,22 +23,24 @@ class _SearchPageState extends State<SearchPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   String query = '';
   final List<String> _tags = ['#태그'];
-  String _searchResult = '';
+  List<Map<String, dynamic>> _searchResults = [];
+  int _totalResultsCount = 0; // 검색 결과의 전체 개수를 저장할 변수
+  bool _showMoreButton = true; // "더보기" 버튼을 표시할지 여부를 결정하는 변수
 
-  Future<void> fetchSearchResult(String query) async {
+  Future<void> fetchSearchResult(String query, {bool fetchAll = false}) async {
     if (query.isEmpty) {
       setState(() {
-        _searchResult = "검색어를 입력해주세요.";
+        _searchResults = [];
+        _totalResultsCount = 0;
+        _showMoreButton = true; // 검색어가 비어있으면 "더보기" 버튼을 다시 표시합니다.
       });
       return;
     }
 
     var body = jsonEncode({'name': query});
     final response = await http.post(
-      Uri.parse('http://43.203.61.149/place/find/'),
-      headers: {
-        "Content-Type": "application/json",
-      },
+      Uri.parse('http://43.203.61.149/place/find/'), // 실제 API URL로 변경해주세요.
+      headers: {"Content-Type": "application/json"},
       body: body,
     );
 
@@ -46,45 +49,30 @@ class _SearchPageState extends State<SearchPage> {
       List placesByName = data['places_by_name'];
       List placesByTag = data['places_by_tag'];
 
-      Set<String> matchedNames = {}; // 중복을 방지
+      Set<Map<String, dynamic>> matchedPlaces = {};
 
-      // "places_by_name"에서 "name" 필드가 검색어를 포함하는 장소의 이름을 찾기
       placesByName.forEach((place) {
-        if ((place['name'] as String).contains(query)) {
-          matchedNames.add(place['name']);
-        }
+        matchedPlaces.add(place);
       });
 
-      // "places_by_tag"에서 "tag" 배열 내에 검색어를 포함하는 태그가 있는 장소의 이름을 찾기
       placesByTag.forEach((place) {
-        var tags = place['tag'] as List;
-        for (var tag in tags) {
-          if ((tag['name'] as String).contains(query)) {
-            matchedNames.add(place['name']);
-            break; =
-          }
-        }
+        matchedPlaces.add(place);
       });
 
-      String resultNames = matchedNames.join('\n');
-
+      _totalResultsCount = matchedPlaces.length;
       setState(() {
-        _searchResult = resultNames;
+        _searchResults =
+            fetchAll ? matchedPlaces.toList() : matchedPlaces.take(5).toList();
+        _showMoreButton = !fetchAll; // "더보기"를 클릭하면 버튼을 숨깁니다.
       });
     } else {
-      print('Failed to load search result: ${response.statusCode}');
+      print('검색 실패 오류코드: ${response.statusCode}');
       setState(() {
-        _searchResult =
-            'Failed to load search result. Error code: ${response.statusCode}';
+        _searchResults = [];
+        _totalResultsCount = 0;
+        _showMoreButton = true;
       });
     }
-  }
-
-  dynamic myEncode(dynamic item) {
-    if (item is DateTime) {
-      return item.toIso8601String();
-    }
-    return item;
   }
 
   @override
@@ -92,29 +80,28 @@ class _SearchPageState extends State<SearchPage> {
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Row(
-          children: [
-            Expanded(
-              child: TextField(
-                onChanged: (value) {
-                  setState(() {
-                    query = value;
-                  });
-                  if (query.isNotEmpty) {
-                    fetchSearchResult(query);
-                  }
-                },
-                decoration: InputDecoration(
-                  hintText: "Search",
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
-                  ),
-                ),
+        automaticallyImplyLeading: false, // 뒤로가기 버튼 숨기기
+        title: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  query = value;
+                });
+              },
+              onSubmitted: (value) => fetchSearchResult(query),
+              decoration: InputDecoration(
+                hintText: "Search",
+                border: InputBorder.none,
+                prefixIcon: Icon(Icons.search),
               ),
             ),
-          ],
+          ),
         ),
       ),
       body: Column(
@@ -128,13 +115,30 @@ class _SearchPageState extends State<SearchPage> {
             ),
           ),
           Expanded(
-            child: Center(
-              child: Text(
-                _searchResult,
-                style: TextStyle(fontSize: 24),
-              ),
+            child: ListView.separated(
+              padding: const EdgeInsets.all(8.0),
+              itemCount: _searchResults.length,
+              separatorBuilder: (BuildContext context, int index) => Divider(),
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(
+                    _searchResults[index]['name'],
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PlaceDetailPage(
+                            placeDetails: _searchResults[index]),
+                      ),
+                    );
+                  },
+                );
+              },
             ),
           ),
+          if (_showMoreButton && _totalResultsCount > 5) _buildMoreButton(),
         ],
       ),
     );
@@ -147,6 +151,18 @@ class _SearchPageState extends State<SearchPage> {
         label: Text(tag),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMoreButton() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Center(
+        child: OutlinedButton(
+          child: Text('더보기'),
+          onPressed: () => fetchSearchResult(query, fetchAll: true),
         ),
       ),
     );
